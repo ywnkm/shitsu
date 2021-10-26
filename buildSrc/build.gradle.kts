434b422b65dfd6652cda3a49022ddbca42fbbb72
ywnkm <ywnkm@foxmail.com>
2021-10-26 22:51:20 +0800

    buildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    maven("https://maven.aliyun.com/nexus/content/groups/public/")
+    mavenCentral()
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+
+    const val kotlin = "1.5.30"
+    const val serialization = "1.3.0-RC"
+    const val coroutines = "1.5.1"
+    const val atomifu = "0.16.2"
+    const val ktor = "1.6.2"
+
+    const val snakeyaml = "1.29"
+}
