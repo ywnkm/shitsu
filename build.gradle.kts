e96c898c16b5b9001f9ae9e9876b3db06783a5d8
ywnkm <ywnkm@foxmail.com>
2021-10-26 22:51:26 +0800

    buildSrc

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+
+buildscript {
+
+    repositories {
+        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+        mavenCentral()
+    }
+
+    dependencies {
+
+    }
+}
+
+allprojects {
+
+    repositories {
+        mavenCentral()
+    }
+}
